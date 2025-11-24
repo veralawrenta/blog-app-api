226c29c0fcb77e5398d2159adabc86ebe9e4673d
veralawrenta <veramlawrenta@gmail.com>
2025-11-24 16:08:34 +0700

    add feature register

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+
+export class AuthController {
+    authService: AuthService;
+
+    constructor() {
+        this.authService = new AuthService();
+    }
+    register = async (req: Request, res: Response) => {
+        const result = await this.authService.register(req.body);
+        return res.status(200).send(result);
+    }
+}
